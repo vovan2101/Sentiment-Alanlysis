f9df822ee2c4de5aa87e0edd71f972171f165446
Vladimir <veliseykin2000@gmail.com>
2022-10-15 14:07:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from msilib.schema import Class
 from urllib import response
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
@@ -16,7 +15,23 @@ for ticker in tickers:
     html = BeautifulSoup(response, 'html')
     news_table = html.find(id='news-table')
     news_tables[ticker] = news_table
-
     break
 
-print(news_tables)
+parsed_data = []
+
+for ticker, news_table in news_tables.items():
+    for row in news_table.findAll('tr'):
+            title = row.text
+            parsed_data.append([ticker, title])
+        
+print(parsed_data)
+
+
+
+# amzn_data = news_tables['AMZN']
+# amzn_rows = amzn_data.findAll('td')
+
+
+# for index, row in enumerate(amzn_rows):
+#     title = row.text
+#     print(title)
